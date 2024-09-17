d999dc1a34dce98a9988cc6156b179efbca78def
Shubham Agrawal <agrlshubham@gmail.com>
2024-09-17 23:15:38 +0530

    Initialised the Project

---
+++ app/src/main/java/com/example/smartbatterymonitor/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.smartbatterymonitor
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.smartbatterymonitor.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+    }
+}
